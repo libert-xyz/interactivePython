eaf586fd4d3bc78d0498c20c8231595482805580
rschmidtz <rschmidt@libert.xyz>
2015-12-05 00:12:32 -0500

    revString2

--- revstring.py
+++ revstring.py
@@ -1,15 +1,34 @@
 #Write a function revstring(mystr) that uses a stack to reverse the characters in a string.
 
+from stackLesson import Stack 
+import time
 
 def revstring(mystr):
 
+	start = time.time()
+
+	myStack = Stack()
+	for ch in mystr:
+		myStack.push(ch)
+	rev = ''
+	while not myStack.isEmpty():
+		rev = rev + myStack.pop()
 	
+	end = time.time()
+	return rev, 'Time: ', end-start
+
+
+def revstring1(mystr):
+	
+	start = time.time()
 	l = []
 	for i in mystr:
 		l = i.split() + l
 	
 	rev = ''.join(l)
-	return rev
+	end = time.time()
+	return rev, 'Time: ', end-start
+
 
 	
 	
